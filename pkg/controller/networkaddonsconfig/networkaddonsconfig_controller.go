2c8db1f72580ad949039957d43a1cef95869c613
João Vilaça <jvilaca@redhat.com>
2023-10-16 19:05:01 +0100

    Refactor metrics
    Signed-off-by: João Vilaça <jvilaca@redhat.com>

--- pkg/controller/networkaddonsconfig/networkaddonsconfig_controller.go
+++ pkg/controller/networkaddonsconfig/networkaddonsconfig_controller.go
@@ -39,7 +39,7 @@ import (
 	"github.com/kubevirt/cluster-network-addons-operator/pkg/apply"
 	"github.com/kubevirt/cluster-network-addons-operator/pkg/controller/statusmanager"
 	"github.com/kubevirt/cluster-network-addons-operator/pkg/eventemitter"
-	"github.com/kubevirt/cluster-network-addons-operator/pkg/monitoring"
+	"github.com/kubevirt/cluster-network-addons-operator/pkg/monitoring/metrics"
 	"github.com/kubevirt/cluster-network-addons-operator/pkg/names"
 	"github.com/kubevirt/cluster-network-addons-operator/pkg/network"
 	"github.com/kubevirt/cluster-network-addons-operator/pkg/util/k8s"
@@ -221,7 +221,7 @@ func (r *ReconcileNetworkAddonsConfig) Reconcile(ctx context.Context, request re
 			r.stopTrackingObjects()
 
 			if r.clusterInfo.MonitoringAvailable {
-				monitoring.ResetMonitoredComponents()
+				metrics.ResetMonitoredComponents()
 			}
 
 			// Owned objects are automatically garbage collected. Return and don't requeue
@@ -313,7 +313,7 @@ func (r *ReconcileNetworkAddonsConfig) Reconcile(ctx context.Context, request re
 	r.statusManager.SetFromPods()
 
 	if r.clusterInfo.MonitoringAvailable {
-		monitoring.TrackMonitoredComponents(&networkAddonsConfig.Spec, r.statusManager)
+		metrics.TrackMonitoredComponents(&networkAddonsConfig.Spec, r.statusManager)
 	}
 
 	// Kubernetes sometimes fails to apply objects while we remove and recreate
--- pkg/monitoring/metrics.go
+++ pkg/monitoring/metrics.go
@@ -7,12 +7,9 @@ import (
 	"strconv"
 
 	"github.com/pkg/errors"
-	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/controller-runtime/pkg/metrics"
 
-	cnao "github.com/kubevirt/cluster-network-addons-operator/pkg/apis/networkaddonsoperator/shared"
-	"github.com/kubevirt/cluster-network-addons-operator/pkg/controller/statusmanager"
 	"github.com/kubevirt/cluster-network-addons-operator/pkg/render"
 )
 
@@ -23,68 +20,6 @@ const (
 	defaultRunbookURLTemplate  = "https://kubevirt.io/monitoring/runbooks/"
 	runbookURLTemplateEnv      = "RUNBOOK_URL_TEMPLATE"
 )
-
-type MetricsOpts struct {
-	Name string
-	Help string
-	Type string
-}
-
-type MetricsKey string
-
-const (
-	ReadyGauge     MetricsKey = "readyGauge"
-	KMPDeployGauge MetricsKey = "kmpDeployedGauge"
-)
-
-var MetricsOptsList = map[MetricsKey]MetricsOpts{
-	ReadyGauge: {
-		Name: "kubevirt_cnao_cr_ready",
-		Help: "CNAO CR Ready",
-		Type: "Gauge",
-	},
-	KMPDeployGauge: {
-		Name: "kubevirt_cnao_cr_kubemacpool_deployed",
-		Help: "KubeMacpool is deployed by CNAO CR",
-		Type: "Gauge",
-	},
-}
-
-var (
-	readyGauge = prometheus.NewGauge(
-		prometheus.GaugeOpts{
-			Name: MetricsOptsList[ReadyGauge].Name,
-			Help: MetricsOptsList[ReadyGauge].Help,
-		})
-	kmpDeployedGauge = prometheus.NewGauge(
-		prometheus.GaugeOpts{
-			Name: MetricsOptsList[KMPDeployGauge].Name,
-			Help: MetricsOptsList[KMPDeployGauge].Help,
-		})
-)
-
-func init() {
-	metrics.Registry.MustRegister(readyGauge, kmpDeployedGauge)
-}
-
-func setGaugeParam(setTrueFlag bool, gaugeParam *prometheus.Gauge) {
-	if setTrueFlag {
-		(*gaugeParam).Set(1)
-	} else {
-		(*gaugeParam).Set(0)
-	}
-}
-
-func ResetMonitoredComponents() {
-	setGaugeParam(false, &readyGauge)
-	setGaugeParam(false, &kmpDeployedGauge)
-}
-
-func TrackMonitoredComponents(conf *cnao.NetworkAddonsConfigSpec, statusManager *statusmanager.StatusManager) {
-	isKubemacpoolDeployed := conf.KubeMacPool != nil
-	setGaugeParam(isKubemacpoolDeployed, &kmpDeployedGauge)
-	setGaugeParam(statusManager.IsStatusAvailable(), &readyGauge)
-}
 
 func RenderMonitoring(manifestDir string, monitoringAvailable bool) ([]*unstructured.Unstructured, error) {
 	if !monitoringAvailable {
